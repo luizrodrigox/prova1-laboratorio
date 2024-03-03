2df9f9dc29b0d58d788f54e835d713be0ed9f7a9
Victor Nunes <victor.nuness787@gmail.com>
2024-03-03 18:03:26 -0300

    q8 completa

---
+++ q8.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+
+      int invertida_inteiro(int num) { 
+int invertida_num=0; 
+
+   While (num!=0){ 
+invertida_num= invertida_num*10+n%10; 
+} 
+   return invertida_num; 
+} 
+
+     int main ( ){ 
+  int num=1234; 
+int invertida = invertida_inteiro(num); 
+
+Printf("Invertida: %d\n", invertida); 
+return 0; 
+}
