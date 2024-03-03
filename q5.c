6b87baedcde8be1132361aee52ee2cea64a3b1cf
Maria Fernanda <mariafernandaitau@gmail.com>
2024-03-03 15:41:11 -0300

    q5itemb.c

--- q5.c
+++ q5.c
@@ -22,3 +22,32 @@ int main() {
 
   return 0;
 }
+
+//Item b)
+
+#include <stdio.h>
+#include <math.h>
+
+void calculaRaizesReais(int a, int b, int c){
+  int delta = b * b - 4 * a * c;
+
+  if(delta>=0){
+    double raiz1 = (- b + sqrt(delta))/(2.0*a);
+    double raiz2 = (- b - sqrt(delta))/(2.0*a);
+
+    printf("As raízes reais da função do segundo grau são: %.2lf e %.2lf", raiz1, raiz2);
+  } else{
+    printf("A função do segundo não tem raizes reais.\n");
+  }
+}
+
+int main(){
+  int a, b, c;
+
+  printf("Digite os coeficiente (a, b, c) da função do segundo grau:\n");
+  scanf("%d %d %d", &a, &b, &c);
+
+  calculaRaizesReais(a, b,c);
+
+  return 0;
+}
