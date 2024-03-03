d8f4c003b65d7b154f3c81551a2309ba08bd6a37
Maria Fernanda <mariafernandaitau@gmail.com>
2024-03-03 11:03:56 -0300

    incompleto q.4

---
+++ q4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void converteParaRomano(int numero){
+    
+}
+
+int main (){
+    printf("Tabela de equivalentes de algarismos romanos para números decimais (1 a 100):\n");
+    for(int i= 1; i<= 100; i++){
+        converteParaRomano(i);
+    }
+    return 0;
+}
