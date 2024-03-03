5dd1b2e2d5ed58b14b516331d612fe2b24502faf
Maria Fernanda <mariafernandaitau@gmail.com>
2024-03-03 17:19:12 -0300

    void 3

--- q3.c
+++ q3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-void encontraTriplosPitagoricos(int n);
+void encontraTriplosPitagoricos(int n){
+  printf("Triplos Pitagóricos até %d:\n", n);
+  for (int cateto1 = 1; cateto1 <= n; cateto1++) {
+    for (int cateto2 = cateto1; cateto2 <= n; cateto2++) {
+      for (int hipotenusa = cateto2; hipotenusa <= n; hipotenusa++) {
+        if (cateto1 * cateto1 + cateto2 * cateto2 == hipotenusa * hipotenusa) {
+          printf("(%d, %d, %d)\n", cateto1, cateto2, hipotenusa);
+        }
+      }
+    }
+  }
+}
 
 int main(){
   int limite;
