abf0678ee93dbcedc0ecba08d93fedbb18e7f6bd
Luiz Rodrigo <melo9657@gmail.com>
2024-03-04 08:13:05 -0300

    questão resolvida

--- q7.c
+++ q7.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
 int isPrimeUtil(int n, int i) {
-    if (n <= 2)
-        return (n == 2) ? 1 : 0;
-    if (i == 1)
-        return 1;
-    if (n % i == 0)
-        return 0;
-    return isPrimeUtil(n, i - 1);
+  if (n <= 2)
+    return (n == 2) ? 1 : 0;
+  if (i == 1)
+    return 1;
+  if (n % i == 0)
+    return 0;
+  return isPrimeUtil(n, i - 1);
 }
 
 int isPrime(int n) {
-    if (n <= 1)
-        return 0;
-    return isPrimeUtil(n, n / 2);
+  if (n <= 1)
+    return 0;
+  return isPrimeUtil(n, n / 2);
 }
 
 int main() {
-    int num;
-    printf("Digite um número para verificar se é primo: ");
-    scanf("%d", &num);
+  int num;
+  printf("Digite um número para verificar se é primo: ");
+  scanf("%d", &num);
 
-    if (isPrime(num))
-        printf("%d é primo.\n", num);
-    else
-        printf("%d não é primo.\n", num);
-    return 0; }
+  if (isPrime(num))
+    printf("%d é primo.\n", num);
+  else
+    printf("%d não é primo.\n", num);
+  return 0; 
+}
--- q8.c
+++ q8.c
@@ -1,18 +1,18 @@
 #include<stdio.h> 
 
-      int invertida_inteiro(int num) { 
-int invertida_num=0; 
+int invertida_inteiro(int num) { 
+   int invertida_num=0; 
 
    While (num!=0);{ 
-invertida_num= invertida_num*10+10; 
-}
+      invertida_num= invertida_num*10+10; 
+   }
    return invertida_num; 
 } 
 
-     int main ( ){ 
-  int num=1234; 
-int invertida = invertida_inteiro(num); 
+int main ( ){ 
+   int num=1234; 
+   int invertida = invertida_inteiro(num); 
 
-Printf("Invertida: %d\n", invertida); 
-return 0; 
+   Printf("Invertida: %d\n", invertida); 
+   return 0; 
 }
