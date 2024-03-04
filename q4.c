9becfc505f3b26bc2774066ca5d93519e2a50c3c
Luiz Rodrigo <melo9657@gmail.com>
2024-03-04 08:56:58 -0300

    questão resolvida

--- q4.c
+++ q4.c
@@ -58,9 +58,9 @@ void converteParaRomano(int numero){
 }
 
 int main (){
-    printf("Tabela de equivalentes de algarismos romanos para números decimais (1 a 100):\n");
-    for(int i= 1; i<= 100; i++){
-        converteParaRomano(i);
-    }
-    return 0;
+  printf("Tabela de equivalentes de algarismos romanos para números decimais (1 a 100):\n");
+  for(int i= 1; i<= 100; i++){
+    converteParaRomano(i);
+  }
+  return 0;
 }
